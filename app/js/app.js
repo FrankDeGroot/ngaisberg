9905bc8e6e1b921f3ca1aee698478cb78914609e
Frank de Groot - Schouten <fjtdg@hotmail.com>
2012-12-24 22:46:56 +0100

    docs-sidebar more mobile friendly but must create nested ng-view and fumble with controllers and layout.

--- app/js/app.js
+++ app/js/app.js
@@ -6,6 +6,7 @@ angular.module('main', ['resProject']).
             when('/', { templateUrl: 'partials/home.html' }).
             when('/about', { templateUrl: 'partials/about.html' }).
             when('/contact', { templateUrl: 'partials/contact.html' }).
+            when('/profile', { templateUrl: 'partials/profile.html' }).
             when('/project', { controller: ProjectListCtrl, templateUrl: 'partials/project/list.html' }).
             when('/project/edit/:projectId', { controller: ProjectEditCtrl, templateUrl: 'partials/project/detail.html' }).
             when('/project/new', { controller: ProjectCreateCtrl, templateUrl: 'partials/project/detail.html' }).
